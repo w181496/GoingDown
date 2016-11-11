3a7fa770803a9ed6b9973886615c7ccee7cca6ec
w181496 <w181496@gmail.com>
2016-11-11 18:15:34 +0800

    first commit

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿using System;
+
+namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(12, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "血量:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(60, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "0";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 30;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.checkBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.checkBox1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.checkBox1.Location = new System.Drawing.Point(109, 30);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(57, 24);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "鎖血";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微軟正黑體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(12, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 19);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "層數:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.Location = new System.Drawing.Point(60, 78);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "0";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(109, 78);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(48, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "+1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(181, 78);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(48, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "+100";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("微軟正黑體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label5.Location = new System.Drawing.Point(12, 113);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 19);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "x座標:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label6.Location = new System.Drawing.Point(60, 112);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(18, 20);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("微軟正黑體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label7.Location = new System.Drawing.Point(105, 112);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(50, 19);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "y座標:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label8.Location = new System.Drawing.Point(157, 111);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(18, 20);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "0";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.checkBox2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.checkBox2.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.checkBox2.Location = new System.Drawing.Point(181, 30);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(73, 24);
+            this.checkBox2.TabIndex = 11;
+            this.checkBox2.Text = "不扣血";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(275, 152);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "糞Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.CheckBox checkBox2;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public int pid;
+        //public int baseAddress = 0x0018FDA0;
+        public int baseAddress = 0x0018FD9C;
+        public int blood_addr, stage_addr, x_addr, y_addr;
+        public int blood, stage, x, y;
+        public bool locked = false;
+        public bool add_blood = false;
+        public string processName = "NS-SHAFT";
+        public byte[] buffer = new byte[4];
+        public IntPtr byteAddress;
+        public Point defPnt = new Point();
+        public IntPtr hwnd;
+
+        public struct Rect
+        {
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
+        }
+
+        [DllImport("kernel32.dll")]
+        public static extern bool ReadProcessMemory
+            (
+                IntPtr hProcess,
+                IntPtr lpBaseAddress,
+                IntPtr lpBuffer,
+                int nSize,
+                IntPtr lpNumberOfBytesRead
+            );
+
+        [DllImportAttribute("kernel32.dll", EntryPoint = "OpenProcess")]
+        public static extern IntPtr OpenProcess
+            (
+                int dwDesiredAccess,
+                bool bInheritHandle,
+                int dwProcessId
+            );
+
+        [DllImport("kernel32.dll")]
+        private static extern void CloseHandle
+            (
+                IntPtr hObject
+            );
+
+        [DllImportAttribute("kernel32.dll", EntryPoint = "WriteProcessMemory")]
+        public static extern bool WriteProcessMemory
+            (
+                IntPtr hProcess,
+                IntPtr lpBaseAddress,
+                int[] lpBuffer,
+                int nSize,
+                IntPtr lpNumberOfBytesWritten
+            );
+
+        [DllImport("user32.dll")]
+        static extern bool GetCursorPos
+            (
+                ref Point lpPoint
+            );
+
+        [DllImport("user32.dll")]
+        private static extern int GetWindowRect
+            (
+                IntPtr hwnd, out Rect lpRect
+            );
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr FindWindow
+            (
+                string strclassName, 
+                string strWindowName
+            );
+
+        public static int GetPid(string windowTitle)
+        {
+            int rs = 0;
+            Process[] arrayProcess = Process.GetProcesses();
+            foreach (Process p in arrayProcess)
+            {
+                if (p.MainWindowTitle.IndexOf(windowTitle) != -1)
+                {
+                    rs = p.Id;
+                    break;
+                }
+            }
+
+            return rs;
+        }
+        public static int GetPidByProcessName(string processName)
+        {
+            Process[] arrayProcess = Process.GetProcessesByName(processName);
+
+            foreach (Process p in arrayProcess)
+            {
+                return p.Id;
+            }
+            return 0;
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            try
+            {
+                hwnd = FindWindow(null, "NS-SHAFT");
+                byteAddress = Marshal.UnsafeAddrOfPinnedArrayElement(buffer, 0); //獲取緩衝區位址
+                pid = GetPid(processName);
+                IntPtr hProcess = OpenProcess(0x1F0FFF, false, pid);
+                ReadProcessMemory(hProcess, (IntPtr)baseAddress, byteAddress, 4, IntPtr.Zero);
+                blood_addr = Marshal.ReadInt32(byteAddress) + 0x1170;
+                stage_addr = Marshal.ReadInt32(byteAddress) + 0x1300;
+                x_addr = Marshal.ReadInt32(byteAddress) + 0x1158;
+                y_addr = Marshal.ReadInt32(byteAddress) + 0x115c;
+                CloseHandle(hProcess);
+            }
+            catch
+            {
+                label1.Text = "GG";
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            IntPtr hProcess = OpenProcess(0x1F0FFF, false, pid);
+            if (add_blood)
+            {
+                WriteProcessMemory(hProcess, (IntPtr)0x00406312, new int[] { 0xAC }, 1, IntPtr.Zero);
+                add_blood = false;
+            }
+            else
+            {
+                WriteProcessMemory(hProcess, (IntPtr)0x00406312, new int[] { 0x84 }, 1, IntPtr.Zero);
+                add_blood = true;
+            }
+            CloseHandle(hProcess);
+        }
+
+        private void Timer1_Tick(object Sender, EventArgs e)
+        {
+            try
+            {
+                IntPtr hProcess = OpenProcess(0x1F0FFF, false, pid);
+
+                ReadProcessMemory(hProcess, (IntPtr)blood_addr, byteAddress, 4, IntPtr.Zero);
+                blood = Marshal.ReadInt32(byteAddress);
+                ReadProcessMemory(hProcess, (IntPtr)stage_addr, byteAddress, 4, IntPtr.Zero);
+                stage = Marshal.ReadInt32(byteAddress);
+                ReadProcessMemory(hProcess, (IntPtr)x_addr, byteAddress, 4, IntPtr.Zero);
+                x = Marshal.ReadInt32(byteAddress);
+                //WriteProcessMemory(hProcess, (IntPtr)y_addr, new int[] { 190 }, 4, IntPtr.Zero);
+                ReadProcessMemory(hProcess, (IntPtr)y_addr, byteAddress, 4, IntPtr.Zero);
+                y = Marshal.ReadInt32(byteAddress);
+
+                Rect rect = new Rect();
+                GetWindowRect(hwnd, out rect);
+                GetCursorPos(ref defPnt);
+                WriteProcessMemory(hProcess, (IntPtr)x_addr, new int[] { defPnt.X - rect.Left - 30}, 4, IntPtr.Zero);
+                WriteProcessMemory(hProcess, (IntPtr)y_addr, new int[] { defPnt.Y - rect.Top - 100}, 4, IntPtr.Zero);
+
+
+                label2.Text = blood.ToString();
+                label4.Text = stage.ToString();
+                label6.Text = x.ToString();
+                label8.Text = y.ToString();
+                if (locked)
+                {
+                    WriteProcessMemory(hProcess, (IntPtr)blood_addr, new int[] { 12 }, 4, IntPtr.Zero);
+                }
+                CloseHandle(hProcess);
+            }
+            catch
+            {
+                label1.Text = "GG";
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IntPtr hProcess = OpenProcess(0x1F0FFF, false, pid);
+            ReadProcessMemory(hProcess, (IntPtr)stage_addr, byteAddress, 4, IntPtr.Zero);
+            stage = Marshal.ReadInt32(byteAddress);
+            WriteProcessMemory(hProcess, (IntPtr)stage_addr, new int[] { stage + 1 }, 4, IntPtr.Zero);
+            CloseHandle(hProcess);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            IntPtr hProcess = OpenProcess(0x1F0FFF, false, pid);
+            ReadProcessMemory(hProcess, (IntPtr)stage_addr, byteAddress, 4, IntPtr.Zero);
+            stage = Marshal.ReadInt32(byteAddress);
+            WriteProcessMemory(hProcess, (IntPtr)stage_addr, new int[] { stage + 100 }, 4, IntPtr.Zero);
+            CloseHandle(hProcess);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBox1.Checked == true)
+            {
+                IntPtr hProcess = OpenProcess(0x1F0FFF, false, pid);
+                WriteProcessMemory(hProcess, (IntPtr)blood_addr, new int[] { 200 }, 4, IntPtr.Zero);
+                CloseHandle(hProcess);
+                locked = true;
+            }
+            else
+            {
+                locked = false;
+            }
+        }
+    }
+}
